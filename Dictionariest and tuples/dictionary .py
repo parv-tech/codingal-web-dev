9dfba0fca8ad48e6ec49845a211d989409d06437
parv-tech <gillpreeti1988@gmail.com>
2025-09-20 11:15:02 +0100

    Add files via upload

---
+++ Dictionariest and tuples/dictionary .py
@@ -0,0 +1,14 @@
+
+my_dict = {"Name" : "Joyce" , "Age" : 17  , "weight" : 25.32 ,  "student" : False }
+
+print(my_dict . get("Name"))
+
+my_dict ["height"] = 5.4
+
+my_dict ["Age"] = 18
+
+my_dict . pop ("weight")
+
+my_dict.clear()
+
+print(my_dict)
---
+++ Dictionariest and tuples/tuple .py
@@ -0,0 +1,3 @@
+my_tuple = ("Joyce" , 20 , 30.5)
+print(my_tuple[1 : 3])
+print(my_tuple[0])
