4a712748b383d74bd3ca5ac9f564fddad3202a16
parv-tech <gillpreeti1988@gmail.com>
2025-09-21 11:20:24 +0100

    Add files via upload

---
+++ Python refresher/fibunacci series .py
@@ -0,0 +1,17 @@
+def fibonacci_series(n):
+    
+    a, b = 0, 1
+
+    series = []
+
+    for _ in range(n):
+
+        series.append(a)
+
+        a, b = b, a + b
+
+    return series
+
+# Example usage
+
+print(fibonacci_series(10))
---
+++ Python refresher/star pattern .py
@@ -0,0 +1,6 @@
+print("star patern")
+
+for i in range(5) : 
+    for p in range(i + 1) :
+        print("*" , end="")
+    print()
